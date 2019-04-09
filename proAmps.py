87a776b7807484df753e9bbd63866cbffd51022b
SantiagoSanchezF <42723108+SantiagoSanchezF@users.noreply.github.com>
2019-04-09 16:09:41 -0500

    Add files via upload
    Upload ensemble models

--- proAmps.py
+++ proAmps.py
@@ -1,9 +1,8 @@
 '''				-- Jose Santiago Sanchez Fragoso --
 
-    proAmps   - Antiicrobial propetide cleavage site predictor and Antimicrobial peptides classificator
+    proAmps   - Antimicrobial propetide cleavage site predictor and Antimicrobial peptides classificator
 
         USAGE: 
-            
             proAmps.py -i <FASTA> [OPTIONS]
 
         OPTIONS:
@@ -36,40 +35,39 @@
             python proAmps.py -i test.fasta -o ./annotated_sequences.fasta 
 '''
 
-import proAmps_module
-import sys, os, getopt
+import sys, os, getopt,textwrap
 
 libpath = os.path.dirname(os.path.realpath(sys.argv[0]))
 def main(argv):
     try:
        opts, args = getopt.getopt(argv,"hcpi:o:",["ifile=","ofile="])
     except getopt.GetoptError:
-       print('proAmps_module.py -i <FASTA> [OPTIONS]')
+       print_help()
        sys.exit(2)
     flag = 0
     flagO = 0
     flagG = 0
     for opt, arg in opts:
-       if opt == "-i":
+       if opt == "-h":
+           print_help()
+           sys.exit()
+       elif opt == "-i":
           flagG = 1
           fasta = arg
-       elif opt == '-h':
-          print('proAmps_module.py -i <FASTA> [OPTIONS]')
-          sys.exit()
        elif opt == "-c":
-           cs = proAmps_module.CSpredictor(fasta,libpath + "/N_ter_proAmp.h5")         
            flag = "c"
        elif opt == "-p":
-           ap = proAmps_module.AMPpredictor(fasta,libpath + "/LASTM_acc_chem.h5")
-           ap.pred_from_propeps(libpath + "/N_ter_proAmp.h5")
            flag = "p"
        elif opt == "-o":
            flagO = 1
            outfile = arg
     if flagG == 0:
-        print('proAmps_module.py -i <FASTA> [OPTIONS]')
+        print_help()
         sys.exit()
+
+    import proAmps_module
     if flag == "c":
+        cs = proAmps_module.CSpredictor(fasta, libpath + "/CS_N_dnn.h5",libpath + "/CS_N_rf.pkl" , libpath+ "/CS_N_svm.pkl",  libpath + "/CS_C_dnn.h5", libpath + "/CS_C_rf.pkl", libpath + "/CS_C_svm.pkl")         
         if flagO == 1:
             tem = sys.stdout
             sys.stdout = f = open(outfile, 'w')
@@ -81,6 +79,8 @@ def main(argv):
             cs.print_fasta()
             sys.exit()
     elif flag == "p":
+        ap = proAmps_module.AMPpredictor(fasta,libpath + "/proAmps.h5",libpath + "/proAmps_rf.pkl")
+        ap.pred_from_propeps( libpath + "/CS_N_dnn.h5",libpath + "/CS_N_rf.pkl" , libpath+ "/CS_N_svm.pkl",  libpath + "/CS_C_dnn.h5", libpath + "/CS_C_rf.pkl", libpath + "/CS_C_svm.pkl")
         if flagO == 1:
             tem = sys.stdout
             sys.stdout = f = open(outfile, 'w')
@@ -92,7 +92,7 @@ def main(argv):
             ap.print_fasta() 
             sys.exit()
     elif flag == 0:
-        ap = proAmps_module.AMPpredictor(fasta,libpath + "/proAmp.h5")
+        ap = proAmps_module.AMPpredictor(fasta,libpath + "/proAmps.h5",libpath + "/proAmps_rf.pkl")
         ap.predict_amps()
         if flagO == 1:
             tem = sys.stdout
@@ -105,6 +105,48 @@ def main(argv):
             ap.print_fasta()
             sys.exit()
 
+
+
+def print_help():
+        print(textwrap.dedent("""\    proAmps   - Antimicrobial propetide cleavage site predictor and Antimicrobial peptides classificator
+
+        USAGE: 
+            proAmps.py -i <FASTA> [OPTIONS]
+
+        OPTIONS:
+
+            -h ; help
+            -i <INPUT FASTA>; mandatory
+            -c ; predict mature sequeces with out AMP prediction
+            -p ; predict mature sequences and predict AMP with mature sequences; 
+                    if not specified prediction of AMP id done assuming mature peptides are given
+            -o <OUTPUT FILE>; indicate the name and path of the output fasta qith the anotation given by proAmps, DEFAULT: results are directed to STDOUT 
+
+        DEPENDENCIES:
+            Keras 2.2 (Python)
+            SciKitlearn (Phython)
+            Pandas (Python)
+            Numpy (Python)
+            importr library (Python)
+            R       v > 3.0
+            Peptides package (R)
+      
+        OTHERS:
+            proAmps.py, proAmpsLib.py, N_ter_proAmp.h5, and proAmp.h5 must be in the same directory when executed.
+
+        EXAMPLES:
+            #Predict mature AMPs and direct results to file
+            python proAmps.py -i test.fasta -c -o ./mature_sequences.fasta
+     
+            #Predict AMPs with no mature sequence prediction, direct results to file
+            #(no AMPs will be predicted as they are not mature sequences) 
+            python proAmps.py -i test.fasta -o ./annotated_sequences.fasta 
+            
+            #Predict AMPs predictiong also mature sequences AMP
+            #print result to STDOUT
+            python proAmps.py -i test.fasta -p
+
+		"""))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- proAmps_module.py
+++ proAmps_module.py
@@ -17,13 +17,13 @@
                     cs.print_fasta()
 
                     #AMP predictor given mature sequences
-                    ap = proAmps.AMPpredictor("test.fasta","/home/fragoso/machineLearning/sem4/deepLearning/LAST_acc.h5")
+                    ap = proAmps.AMPpredictor("test.fasta","proAmps.h5","proAmps_rf.pkl")
                     ap.predict_amps()
                     ap.print_fasta()
 
-					#AMP predictor given AMP pro-peptides
-					ap = proAmps.AMPpredictor("test.fasta","/home/fragoso/machineLearning/sem4/deepLearning/LAST_acc.h5")
-					ap.pred_from_propeps("/home/fragoso/machineLearning/sem4/cleaveSite/good_models/N_two_dense_concat_tr-val.h5")
+                    #AMP predictor given AMP pro-peptides
+                    ap = proAmps.AMPpredictor("test.fasta","proAmps.h5","proAmps_rf.pkl")
+                    ap.pred_from_propeps("/home/fragoso/machineLearning/sem4/cleaveSite/good_models/N_two_dense_concat_tr-val.h5")
                     ap.print_fasta()
 '''
 
@@ -32,46 +32,51 @@ import pandas as pd
 import numpy as np
 from rpy2.robjects.packages import importr
 from keras import models
+from sklearn.externals import joblib
 
 
 '''    CLASSES    '''
 class AMPpredictor:
-    def __init__(self,fasta,model):
+    def __init__(self,fasta,model,model_rf):
         self.df = {}
         self.fasta = fasta
         self.model = model
-    def pred_from_propeps(self,modelN):
-        CS = CSpredictor(self.fasta,modelN)
+        self.model_rf = model_rf
+    def pred_from_propeps(self,modelN_dl,modelN_rf,modelN_svm,modelC_dl,modelC_rf,modelC_svm):
+        CS = CSpredictor(self.fasta,modelN_dl,modelN_rf,modelN_svm,modelC_dl,modelC_rf,modelC_svm)
         self.df = CS.df
-        self.df["y_pred"], self.df["annot"] = pred_AMP(self.df["mature"],self.model)
+        self.df["y_pred"], self.df["annot"] = pred_AMP(self.df["mature"],self.model,self.model_rf)
         return self.df["y_pred"]
     def predict_amps(self):
         self.df["ids"],self.df["seqs"] = fasta_to_df(self.fasta)
-        self.df["y_pred"], self.df["annot"] = pred_AMP(self.df["seqs"],self.model)
+        self.df["y_pred"], self.df["annot"] = pred_AMP(self.df["seqs"],self.model,self.model_rf)
         return self.df["y_pred"]
     def print_fasta(self):
         try:
             if self.df["mature"]:
                 for i in range(len(self.df["ids"])):
-                    print(">" + self.df["ids"][i] + " | " + self.df['mature_annot'][i] 
-                            + " | " + self.df["annot"][i]
+                    print(">" + self.df["ids"][i] + "    |    " + self.df['mature_annot'][i] 
+                            + "    |    " + self.df["annot"][i]
                             + "\n" + self.df['mature'][i] )
         except:
             for i in range(len(self.df["ids"])):
-                print(">" + self.df["ids"][i] + " | " + self.df["annot"][i]
+                print(">" + self.df["ids"][i] + "    |    " + self.df["annot"][i]
                         + "\n" + self.df['seqs'][i] )
 
 class CSpredictor:
-    def __init__(self,fasta,modelN):
+    def __init__(self,fasta,modelN_dl,modelN_rf,modelN_svm,modelC_dl,modelC_rf,modelC_svm):
         self.df = {}
         self.df["ids"],self.df["seqs"] = fasta_to_df(fasta)
-    #def predict_mature_seqs(self,modelN):
-        self.clfN = models.load_model(modelN)
-        self.df['mature'],self.df['mature_annot'] = pred_mature_from_list(self.df["seqs"],self.clfN)
-    #    return self.df['mature'], self.df['mature_annot']
+        self.clfN = models.load_model(modelN_dl)
+        self.modelN_rf = joblib.load(modelN_rf) 
+        self.modelN_svm = joblib.load(modelN_svm) 
+        self.clfC = models.load_model(modelC_dl)
+        self.modelC_rf = joblib.load(modelC_rf) 
+        self.modelC_svm = joblib.load(modelC_svm) 
+        self.df['mature'],self.df['mature_annot'] = pred_mature_from_list(self.df["seqs"],self.clfN,self.modelN_rf,self.modelN_svm,self.clfC,self.modelC_rf,self.modelC_svm)
     def print_fasta(self):
         for i in range(len(self.df["ids"])):
-            print(">" + self.df["ids"][i] + " | " + self.df['mature_annot'][i] 
+            print(">" + self.df["ids"][i] + "    |    " + self.df['mature_annot'][i] 
                     + "\n" + self.df['mature'][i] )
 
 
@@ -88,20 +93,24 @@ def encode_seqs(seqs):
             results[i, j, index] = 1.
     return results
 
-def pred_AMP(seqs,model):        
+def pred_AMP(seqs,model,model_rf):        
         pep = importr('Peptides')
         ohe_seqs = encode_seqs(seqs)
         chem_array = pd.DataFrame(calc(seqs,pep)).values
         chem_array -= chem_mean
         chem_array /= chem_std
         model = models.load_model(model)
-        probs = model.predict([chem_array,ohe_seqs])
-        annot = [" no-AMP " if x < 0.505 else "predicted AMP with a probabiity of: " + str(np.round(x,3)) for x in probs]
-        y_pred = [0 if x < 0.505 else 1 for x in probs]
+        model_rf = joblib.load(model_rf)
+        probs_dl = model.predict([chem_array,ohe_seqs])
+        probs_dl = probs_dl.reshape(probs_dl.shape[0])
+        probs_rf = np.array([x for x in zip(*model_rf.predict_proba(chem_array ))][1])
+        probs = 0.61*probs_dl + 0.39*probs_rf
+        annot = ["non-AMP with a probabiity of: " + str(np.round(x,3)) if x < 0.505 else "predicted AMP with a probabiity of: " + str(np.round(x,3)) for x in probs]
+        y_pred = [0 if x < 0.5 else 1 for x in probs]
         return y_pred, annot
 
 ## take a sequence and make an np array with OHEing of the octamer, and the noralized position at the end of the vector
-def seqs_to_ohe_octamers(seq):
+def seqs_to_ohe_octamers(seq,mean,std):
     octas = []
     for i in range(len(seq)-7):
         octas.append(seq[i:i+8])
@@ -116,29 +125,44 @@ def seqs_to_ohe_octamers(seq):
     results = results.reshape(results.shape[0],8*21)
     octas = []
     for i in range(len(results)):
-        norm_p = i - 44.65728994300423
-        norm_p /= 83.38561021257111
+        norm_p = i - mean
+        norm_p /= std
         octas.append(np.append(results[i],norm_p))
     octas = np.array(octas)
     return octas
 
 ## makes prediction, returns the position of CS and the probaility 
-def ohe_prediction(octas,model):
-    y_pred = model.predict(octas)                             
-    return np.argmax(y_pred),np.max(y_pred)  
+def ohe_prediction(octas,model_dl,model_rf,model_svm,wei): 
+    y_pred_dl = model_dl.predict(octas) 
+    y_pred_dl = y_pred_dl.reshape(y_pred_dl.shape[0]) 
+    y_pred_rf = np.array([x for x in zip(*model_rf.predict_proba(octas))][1]) 
+    y_pred_svm = np.array([x for x in zip(*model_svm.predict_proba(octas))][1])
+    y_pred = wei[0]*y_pred_dl + wei[1]*y_pred_rf + wei[2]*y_pred_svm
+    return np.argmax(y_pred),np.max(y_pred)
 
 ## predict the mature sequence and return annotation    
-def pred_mature(seq,model):
-    pos,prob = ohe_prediction(seqs_to_ohe_octamers(seq),model)
-    if prob >= 0.5 and len(seq[pos+4:]) > 4:
-        return seq[pos+4:],"N-terminus CS predicted at position: " + str(pos+4) + ", with a probability of: " + str(np.round(prob,3)) 
+def pred_mature(seq,modelN,modelN_rf,modelN_svm,modelC,modelC_rf,modelC_svm):
+    ohe_N = seqs_to_ohe_octamers(seq,43.98830865159782,82.4440045905732)
+    ohe_C = seqs_to_ohe_octamers(seq,35.84697508896797, 56.168662322490874)
+    #pos_N,prob_N  = ohe_prediction(ohe_N,modelN,modelN_rf,modelN_svm,(0.5,0.1,0.4))
+    pos_N,prob_N  = ohe_prediction(ohe_N,modelN,modelN_rf,modelN_svm,(0,0,1))
+    pos_C,prob_C  = ohe_prediction(ohe_C,modelC,modelC_rf,modelC_svm,(0.02, 0.89, 0.09))
+    
+   # pos_N,prob_N,pos_C,prob_C = ohe_prediction(seqs_to_ohe_octamers(seq),modelN,modelC)
+    if prob_N >= 0.6 and len(seq[pos_N+4:]) > 4:
+        if prob_C >= 0.6 and len(seq[pos_N+4:pos_C+4]) > 4:
+            return seq[pos_N+4:pos_C+4],"N-terminus CS predicted at position: " + str(pos_N+4) + ", with a probability of: " + str(np.round(prob_N,3)) + "    |    " + "C-terminus CS predicted at position: " + str(pos_C+4) + ", with a probability of: " + str(np.round(prob_C,3))
+        else:    
+            return seq[pos_N+4:],"N-terminus CS predicted at position: " + str(pos_N+4) + ", with a probability of: " + str(np.round(prob_N,3)) + "    |    No C-terminus CS predicted"
+    elif prob_C >= 0.6 and len(seq[:pos_C+4]) > 4:
+        return seq[:pos_C+4],"No N-terminus CS predicted    |    C-terminus CS predicted at position: " + str(pos_C+4) + ", with a probability of: " + str(np.round(prob_C,3))
     else:
-        return seq," No N-terminus CS predicted "
+        return seq,"No N-terminus CS predicted    |    No C-terminus CS predicted"
 
-def pred_mature_from_list(seqs,model):
+def pred_mature_from_list(seqs,modelN,modelN_rf,modelN_svm,modelC,modelC_rf,modelC_svm):
     mature, mature_annot = [],[]
     for seq in seqs:
-        m,a = pred_mature(seq,model)
+        m,a = pred_mature(seq,modelN,modelN_rf,modelN_svm,modelC,modelC_rf,modelC_svm)
         mature.append(m)
         mature_annot.append(a)
     return mature, mature_annot
@@ -180,15 +204,15 @@ def calc(seqs,pep):
         df['hmoment160'].append(pep.hmoment(i, angle = 160, window = 11)[0])
     return df
 
-chem_mean = np.array([1.12682036e+01, 8.22427575e+01, 1.03248939e+01, 1.46145475e+01,
-       1.58173564e+00, 8.96893685e+00, 2.58827405e+01, 5.38099076e-01,
-       5.09094550e-01, 4.02319471e+01, 6.07106318e+03, 5.47170148e+01,
-       7.42611746e+00, 5.02547094e+01, 2.98228783e+01])
+chem_mean = np.array([8.42277739e+00, 8.30251263e+01, 1.07009223e+01, 1.47836489e+01,
+       1.35775798e+00, 9.21663941e+00, 2.32064232e+01, 5.21321870e-01,
+       4.94085645e-01, 3.94308015e+01, 5.90153175e+03, 5.71550613e+01,
+       8.09864228e+00, 5.13415030e+01, 3.18229247e+01])
 
-chem_std = np.array([6.89297727e+00, 3.18311848e+01, 6.10230694e+00, 7.24192136e+00,
-       1.12397518e+00, 5.54875196e+00, 9.57769543e+00, 1.65401747e-01,
-       1.61253677e-01, 2.40659253e+01, 3.29296047e+03, 1.06441609e+01,
-       2.62688438e+00, 1.14780823e+01, 1.08356047e+01])
+chem_std = np.array([6.41429619e+00, 3.80489830e+01, 5.84315338e+00, 8.87892116e+00,
+       1.37619010e+00, 6.96499326e+00, 1.06032731e+01, 1.75051327e-01,
+       1.62239968e-01, 2.41712756e+01, 3.26236859e+03, 1.17307177e+01,
+       2.73216063e+00, 1.27165049e+01, 1.19224287e+01])
 
 ## take a fata and extract ids and sequences
 def fasta_to_df(fasta):
